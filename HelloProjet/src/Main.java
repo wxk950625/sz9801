6fefda891e37205ccf28832d82d6416b4913810e
wxk <1316945163@qq.com>
2020-08-17 23:00:02 +0800

    添加好运

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -9,6 +9,6 @@ public class Main {
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
-		你好sj
+		你好sj goodlucklyFOR YOU
     }
 }
