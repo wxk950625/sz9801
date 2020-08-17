e02e649ace655a21fb20e8c597941382da11ae2c
wxk <1316945163@qq.com>
2020-08-17 22:49:35 +0800

    制造版本冲突

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -15,7 +15,6 @@ public class Main {
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
-        System.out.println("HelloWorld!");
 		
     }
 }
