9e7743f1906cb0ff8772ecf1e2adbee0c4e32095
wxk <1316945163@qq.com>
2020-08-17 23:41:08 +0800

    添加五行goodlucky

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -10,6 +10,11 @@ public class Main {
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
 		你好sj goodlucklyFOR YOU;
+		你好sj goodlucklyFOR YOU;
+		你好sj goodlucklyFOR YOU;
+		你好sj goodlucklyFOR YOU;
+		你好sj goodlucklyFOR YOU;
+		你好sj goodlucklyFOR YOU;
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
