8fd5763a448071e384dae5b76b635bbd6553aac3
wxk <1316945163@qq.com>
2020-08-17 23:39:57 +0800

    添加sj

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -17,6 +17,6 @@ public class Main {
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld!");
-		你好世界
+		sj你好 gooklucky
     }
 }
